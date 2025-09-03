7400b4cfe0be237eff2007ec53102d54f3846823
BernardoLomas <bernardo.lomasb@gmail.com>
2025-09-03 17:56:41 -0300

    Calculadora com Constructor function

---
+++ Calculadora-ConstructorFunction/app.js
@@ -0,0 +1,65 @@
+function Calculadora() {
+    this.display = document.querySelector('.display');
+
+    //Inicia a calculadora
+    this.Inicia = function () {
+        this.cliqueBotoes();
+    };
+
+    //Limpa o display
+    this.clearDisplay = function() {
+        this.display.value = '';
+    };
+
+    //Apaga um caractere
+    this.apagaUm = function() {
+        this.display.value = this.display.value.slice(0, -1);
+    };
+
+    //Botão que pega o valor do display
+    this.BtnParaDisplay = function (valor) {
+        this.display.value += valor;
+    };
+
+    //Pega os cliques dos botões
+    this.cliqueBotoes = function () {
+        document.addEventListener('click', (e) => { // É aqui onde o Js vai pegar onde o clique foi
+            const el = e.target;
+
+            if (el.classList.contains('btn-num')) {
+                this.BtnParaDisplay(el.innerText);
+            }
+
+            if (el.classList.contains('btn-clear')) {
+                this.clearDisplay();
+            }
+
+            if (el.classList.contains('btn-del')) {
+                this.apagaUm();
+            }
+
+            if (el.classList.contains('btn-eq')) {
+                this.RealizaConta();
+            }
+        });
+    };
+
+    //Realiza conta da calculadora 
+    this.RealizaConta = function () {
+
+        let conta = this.display.value;
+        try {
+            conta = eval(conta);
+            if (!conta && conta !== 0) {
+                alert('Conta inválida');
+                return;
+            }
+            this.display.value = String(conta);
+        } catch (e) {
+            alert('Conta inválida');
+        }
+    };
+}
+
+const conta = new Calculadora();
+conta.Inicia();
