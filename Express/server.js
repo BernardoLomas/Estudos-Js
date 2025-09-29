63cee46a3380e3e907d47565684baf2daf92888a
BernardoLomas <bernardo.lomasb@gmail.com>
2025-09-29 16:03:05 -0300

    Alterações no express

--- Express/server.js
+++ Express/server.js
@@ -1,16 +1,28 @@
-const express = require('express') //Importação do módulo do express
-const app = express() //Criação da variável app para uso do express
-
 // Uma API deve ser capaz de fazer o CRUD = C: Create (criar), R: Read(ler), U: Update (atualizar), D: Delete(apagar)
 // http://meusite.com/sobre <- GET -> Entregue a página /sobre
+const express = require('express') //Importação do módulo do express
+const app = express() //Criação da variável app para uso do express
+app.use(express.json())
 
 app.get('/', (req, res) => { //Manda a requisição da aba '/' e retorna a resposta 'Hello World!' a partir do método GET
     res.send('Hello World!');
 });
 
-app.get('/contato', (req, res) => {
-    res.send('Obrigado por acessar meu site!');
+app.get('/produto/:id?123', (req, res) => {
+    const id = req.params.id;
+    res.send(`Produto com ID: ${id}`);
 });
+
+app.get('/busca/:categoria=tenis&preco=100', (req, res) => {
+    const categoria = req.query.categoria;
+    const preco = req.query.preco;
+    res.send(`Buscando categoria: ${cateogoria} com preço até: ${preco}`);
+})
+
+app.post('/cadastro', (req, res) => {
+    const {usuario, senha } = req.body;
+    res.send(`Usuário: ${usuario} | Senha: ${senha}`);
+})
 
 app.listen(3000, () => {
     console.log('Acessar http://localhost:3000');
