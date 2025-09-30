f42d38cb759e72acae25bc8beb0f22a037d341a9
BernardoLomas <bernardo.lomasb@gmail.com>
2025-09-30 17:06:01 -0300

    Criação das pastas de frontend

--- Express/server.js
+++ Express/server.js
@@ -3,11 +3,14 @@ const app = express() //Criação da variável app para uso do express
 app.use(express.json()); //Possibilita interpretação de Json pelo express
 const path = require('path');
 
+app.use(express.urlencoded({ extend: true }));
+app.use(express.static(path.join(__dirname, 'src', 'public'))); // Leitura de arquivos estáticos pelo Express
+
 const produtosRoutes = require('./src/Routes/produtosRoutes.Js'); //Exportação do Routes
 app.use('/produtos', produtosRoutes); //Uso dos métodos de produtos pelo express
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Leitura dos views, arquivos HTML por assim dizer
+app.set('view engine', 'ejs'); // Interpretação do EJS views pelo Express
 
 app.get('/home', (req, res) => {
     res.render('index');
