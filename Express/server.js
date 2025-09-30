390e0f25f82128660996d463aa7704b54fcfca66
BernardoLomas <bernardo.lomasb@gmail.com>
2025-09-30 15:34:45 -0300

    Treino de Routes, Controllers e Requisições Get e Post

--- Express/server.js
+++ Express/server.js
@@ -3,6 +3,7 @@
 const express = require('express') //Importação do módulo do express
 const app = express() //Criação da variável app para uso do express
 app.use(express.json())
+const produtosRoutes = require('./Routes/produtosRoutes.Js');
 
 app.get('/', (req, res) => { //Manda a requisição da aba '/' e retorna a resposta 'Hello World!' a partir do método GET
     res.send('Hello World!');
@@ -17,12 +18,14 @@ app.get('/busca', (req, res) => {
     const termo = req.query.termo;
     const pagina = req.query.pagina;
     res.send(`Buscando categoria: ${termo} com preço até: ${pagina}`);
-});
+})
 
 app.post('/cadastro', (req, res) => {
     const {usuario, senha } = req.body;
     res.send(`Usuário: ${usuario} | Senha: ${senha}`);
 });
+
+app.use('/produtos', produtosRoutes);
 
 app.listen(3000, () => {
     console.log('Acessar http://localhost:3000');
